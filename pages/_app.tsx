3fe520b94242ed9b5568dcf4f16ddc558c19f93c
cuong@foxit.co.jp <cuong@foxit.co.jp>
2022-05-24 16:26:58 +0700

    add meta for admin

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,10 +5,15 @@ import { ThemeProvider } from "styled-components";
 import variable from "../styles/variable";
 import { Provider } from "react-redux";
 import { store } from "../store/store";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
+      <Head>
+        <title>Admin Sashimeomeo</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+      </Head>
       <ThemeProvider theme={variable}>
         <NavbarComponent></NavbarComponent>
         <BodyComponent>
