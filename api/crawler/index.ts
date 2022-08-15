112ce6ec6b7db8b0adcd6feaab47a25df60134d8
Nguyen Cuong <cuong@foxit.co.jp>
2022-08-15 21:09:16 +0700

    init

--- api/crawler/index.ts
+++ api/crawler/index.ts
@@ -1,27 +1,18 @@
-
-
-
-
 // import fetch from 'node-fetch';
-import {default as axios } from '../axios';
+import { default as axios } from '../axios';
 
-let url = "/products/getProductShopee";
+let url = '/products/getProductShopee';
+
 const CrawlerAPI_SHOPEE = {
-    getProductByID:async (itemID:string,shopID:string)=>{
-        return axios({
-            url:url,
-            method:"POST",
-            data:{itemID,shopID},
-        })
-
-
-    }
-  
-}
-
-
-
-
+  getProductByID: async (shopeeUrl: String) => {
+    return axios({
+      url: url,
+      method: 'GET',
+      params: {
+        shopeeUrl,
+      },
+    });
+  },
+};
 
 export default CrawlerAPI_SHOPEE;
-
