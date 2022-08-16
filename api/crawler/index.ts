ccd5c9bf207357edafaca7cde91c233c871b2bc0
Cuong <ffgcuong@gmail.com>
2022-08-16 08:52:57 +0700

    add crawler

---
+++ api/crawler/index.ts
@@ -0,0 +1,18 @@
+// import fetch from 'node-fetch';
+import { default as axios } from '../axios';
+
+let url = '/products/getProductShopee';
+
+const CrawlerAPI_SHOPEE = {
+  getProductByID: async (shopeeUrl: String) => {
+    return axios({
+      url: url,
+      method: 'GET',
+      params: {
+        shopeeUrl,
+      },
+    });
+  },
+};
+
+export default CrawlerAPI_SHOPEE;
